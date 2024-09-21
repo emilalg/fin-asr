f2ff83d7ded48969c14a59787715f3229955c956
emilalg <Emil.Algars@metropolia.fi>
2024-09-21 14:17:58 +0300

    Error handling in training loop

--- src/train.py
+++ src/train.py
@@ -1,6 +1,6 @@
 import logging
 import os
-import pickle
+import gc
 import torch
 import torch.nn as nn
 import torch.utils
@@ -26,43 +26,71 @@ def train(model, train_loader, device, optimizer, loss_fn, epoch):
     # set training mode
     model.train()
     total_loss = 0
+    skipped_batches = 0
 
     # run batch and accumulate loss (train)
     for batch_idx, batch in enumerate(tqdm(train_loader, desc=f"Epoch {epoch+1}/{args.epochs} - Training")):
-        audio_padded, labels_padded, audio_lengths, label_lengths = batch
-        
-        audio = torch.stack(audio_padded, dim=0).to(device)
-        # Shape: (batch_size, max_label_length)
-        labels = torch.stack(labels_padded, dim=0).to(device)
-        input_lengths = torch.tensor(audio_lengths).to(device)
-        target_lengths = torch.tensor(label_lengths).to(device)
+        try:
+            audio_padded, labels_padded, audio_lengths, label_lengths = batch
+            
+            audio = torch.stack(audio_padded, dim=0).to(device)
+            # Shape: (batch_size, max_label_length)
+            labels = torch.stack(labels_padded, dim=0).to(device)
+            input_lengths = torch.tensor(audio_lengths).to(device)
+            target_lengths = torch.tensor(label_lengths).to(device)
+            optimizer.zero_grad()
+            
+            # Forward pass
+            outputs = model(audio)
+            
+            # Shape: (max_input_length, batch_size, num_classes)
+            log_probs = F.log_softmax(outputs, dim=2).permute(1, 0, 2)
+            # Compute loss
+            loss = loss_fn(log_probs, labels, input_lengths, target_lengths)
+            loss.backward()
+            
+            # clip gradients
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)  
+            optimizer.step()
+            total_loss += loss.item()
 
-        optimizer.zero_grad()
-        
-        # Forward pass
-        outputs = model(audio)
-        
-        # Shape: (max_input_length, batch_size, num_classes)
-        log_probs = F.log_softmax(outputs, dim=2).permute(1, 0, 2)
+        except torch.cuda.OutOfMemoryError as e:
+            logging.warning(f"Ran out of memory in batch {batch_idx}. "
+                               f"Audio tensor shape: ({audio.shape}). "
+                               f"Skipping batch")
+            skipped_batches += 1
+            continue
 
-        # Compute loss
-        loss = loss_fn(log_probs, labels, input_lengths, target_lengths)
-        loss.backward()
-        
-        # clip gradients
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)  
+        except RuntimeError as e:
+            if "out of memory" in str(e):
+                # Log the shape of the audio tensor
+                logging.warning(f"Ran out of memory in batch {batch_idx}. "
+                               f"Audio tensor shape: ({audio.shape}). "
+                               f"Skipping batch")
+            else:
+                logging.error(f"An unexpected runtime error occurred in batch {batch_idx}: {str(e)}")
+            skipped_batches += 1
+            continue
 
-        optimizer.step()
-        total_loss += loss.item()
+        except Exception as e:
+            logging.error(f"An unexpected error occurred in batch {batch_idx}: {str(e)}")
+            skipped_batches += 1
+            continue
 
-        #perhaps helps avoid oom issue
-        del audio, labels, input_lengths, target_lengths
-        del outputs, log_probs
-        del loss
-        # maybe helps
-        torch.cuda.empty_cache()
+        finally:
+            # Clean up to help avoid OOM issues
+            # Slightly increased runtime, but stabilizes vram usage
+            del audio, labels, input_lengths, target_lengths
+            if 'outputs' in locals():
+                del outputs
+            if 'log_probs' in locals():
+                del log_probs
+            if 'loss' in locals():
+                del loss
+            torch.cuda.empty_cache()
+            gc.collect()
 
-    train_loss = total_loss / len(train_loader)
+    train_loss = total_loss / (len(train_loader)-skipped_batches)
     return train_loss
 
 
@@ -124,6 +152,14 @@ def validate(model, val_loader, device, epoch, loss_fn, alphabet, scheduler, ts)
             total_wer += wer(ref, decoded_text)
             total_cer += cer(ref, decoded_text)
 
+            #perhaps helps avoid oom issue
+            del audio, labels, input_lengths, target_lengths
+            del output, log_probs, log_probs_decode
+            del loss
+            # maybe helps
+            torch.cuda.empty_cache()
+            gc.collect()
+
     val_loss = total_val_loss / len(val_loader)
     cer_out = total_cer / len(val_loader)
     wer_out = total_wer / len(val_loader)
@@ -146,6 +182,11 @@ def main(args):
         raise Exception('Gpu not available')
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logging.info(f"Using device: {device}")
+
+    #for reproducability
+    torch.manual_seed(42)
+
+    #init utils
     alphabet = Alphabet()
     ts = utils.TensorBoardUtils(f'{args.output}log/', args.debug)
 
@@ -176,14 +217,17 @@ def main(args):
         dropout_rate=0.2
     ).to(device)
 
-    #1024 placeholder for max sequence length, computing it would be too expensive
-    #due to the size of the dataset
-    summary(model, input_size=(args.batch_size, 1024, 40))
+    #2048 placeholder for max sequence length, computing it would be too 
+    #expensive due to the size of the dataset
+    summary(model, input_size=(args.batch_size, 2048, 40))
 
     # initialize optimizer, loss_fn, and scheduler
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+
+    #calculate index of blank token to pass to CTCLOSS
     blank_index = len(alphabet.alphabet) - 1
     loss_fn = nn.CTCLoss(blank=blank_index, zero_infinity=True).to(device)
+
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2)
 
     # training loop
